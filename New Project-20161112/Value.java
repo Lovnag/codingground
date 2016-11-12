f13ad58713096e84ebe64ba6e0866d838b3cac56
Makhach I Israpilov <lovnager@gmail.com>
2016-11-12 13:49:59 +0300

    Created by Coding Ground Client

---
+++ New Project-20161112/Value.java
@@ -0,0 +1,11 @@
+public class Value implements Expression{
+    private double value;
+    
+    public Value(double value){
+        this.value=value;
+    }
+    
+    public double calculate(){
+        return this.value;
+    }
+}

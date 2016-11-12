a14cfabcc1ae4eb9c8df5dabc98f4084957a688b
Makhach I Israpilov <lovnager@gmail.com>
2016-11-12 13:49:59 +0300

    Created by Coding Ground Client

---
+++ New Project-20161112/SqrOperation.java
@@ -0,0 +1,11 @@
+public class SqrOperation extends UnaryOperation{
+    
+    public SqrOperation(Expression value){
+        super(value);
+    }
+    
+    protected double doCalculate(Expression expr){
+        double v=value.calculate();
+        return v*v;
+    }
+}
